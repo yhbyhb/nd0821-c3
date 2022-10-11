a778853d4d09892cfdf7a36395bcc525806c00cc
HanByul Yang <hanbyul.yang@gmail.com>
2022-10-11 09:33:50 +0900

    updated train_model.py for slicing data

--- starter/train_model.py
+++ starter/train_model.py
@@ -1,14 +1,22 @@
 # Script to train machine learning model.
+import csv
+import numpy as np
+import os
 import pandas as pd
 import pickle
+import yaml
 from sklearn.model_selection import train_test_split
 from ml.data import process_data
 from ml.model import train_model, compute_model_metrics, inference
 
-data = pd.read_csv("../data/census_cleaned.csv")
+import pathlib
+cur_path = pathlib.Path(__file__).parent.resolve()
 
+data = pd.read_csv(os.path.join(cur_path, "../data/census_cleaned.csv"))
+
+np.random.seed(23)
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(data, test_size=0.20, random_state=23)
 
 cat_features = [
     "workclass",
@@ -20,6 +28,7 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
@@ -33,7 +42,7 @@ X_test, y_test, _, _ = process_data(
 # Train and save a model.
 model = train_model(X_train, y_train)
 
-with open("../model/model.pkl", 'wb') as f:
+with open(os.path.join(cur_path, "../model/model.pkl"), 'wb') as f:
     pickle.dump(model, f)
 
 # Inference and compute metrics
@@ -42,3 +51,34 @@ preds = inference(model, X_test)
 precision, recall, fbeta = compute_model_metrics(y_test, preds)
 print(f'precisio : {precision}, recall : {recall}, fbeta : {fbeta}')
 
+# categorical data slicing
+slicing_perf= []
+df = data
+for feature in cat_features:
+    for feature_value in df[feature].unique():
+        # slicing dataframe with respect to feature and feature_value
+        sliced_df = df[df[feature] == feature_value]
+        X_slice, y_slice, _, _ = process_data(
+            sliced_df,
+            categorical_features=cat_features,
+            label="salary", training=False,
+            encoder=encoder, lb=lb)
+        predictions_slice = inference(model, X_slice)
+        precision_sl, recall_sl, f_beta_sl = compute_model_metrics(y_slice, predictions_slice)
+        slicing_perf_dict = {
+            'feature' : feature,
+            'feature_value' : feature_value,
+            'precision' : precision_sl,
+            'recall' : recall_sl,
+            'f_beta' : f_beta_sl,
+        }
+        slicing_perf.append(slicing_perf_dict)
+        # from pprint import pprint 
+        # pprint(perf_dict)
+      
+keys = slicing_perf[0].keys()
+
+with open(os.path.join(cur_path, '../logs/sliceing.csv'), 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(slicing_perf)
